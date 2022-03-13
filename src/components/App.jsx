cfe435198aca208d961c5cad5a2346fb74ca40e3
Your Name <you@example.com>
2022-03-13 17:37:39 +0530

    சும்மா சாதாரன எழுத்து பலகை

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,15 @@
 import React, { useEffect } from "react";
 import "./index.scss";
-import { characterSet } from "./characterSet";
+import {
+  realConsonantSet,
+  cartesianProductSet,
+  consonantSet,
+  characterSet,
+  topPanel,
+  leftPanal,
+  rightPanel,
+  bottomPanel,
+} from "./characterSet";
 
 const Key = ({ keyitem, onKeySelect }) => {
   return (
@@ -20,13 +29,48 @@ const PadRow = ({ row, onKeySelect }) => {
   );
 };
 
-const KeyPad = ({ keyset, onKeySelect, ...props }) => {
+const Panel = ({ keys, onKeySelect }) => {
+  return (
+    <div className="panel">
+      {keys.map((item) => (
+        <Key keyitem={item} onKeySelect={onKeySelect} />
+      ))}
+    </div>
+  );
+};
+
+const KeyPad = ({
+  keyset,
+  onKeySelect,
+  leftPanelkeys,
+  topPanelkeys,
+  rightPanelkeys,
+  bottomPanelkeys,
+  ...props
+}) => {
   return (
     <div className="keyboard-container">
       <div className="key-bank">
-        {keyset.map((row) => (
-          <PadRow row={row} onKeySelect={onKeySelect} />
-        ))}
+        <div className="key-rows">
+          {keyset.map((row) => (
+            <PadRow row={row} onKeySelect={onKeySelect} />
+          ))}
+        </div>
+      </div>
+
+      <div className="left-panel">
+        <Panel keys={leftPanelkeys} onKeySelect={onKeySelect} />
+      </div>
+
+      <div className="top-panel">
+        <Panel keys={topPanelkeys} onKeySelect={onKeySelect} />
+      </div>
+
+      <div className="right-panel">
+        <Panel keys={rightPanelkeys} onKeySelect={onKeySelect} />
+      </div>
+      <div className="bottom-panel">
+        <Panel keys={bottomPanelkeys} onKeySelect={onKeySelect} />
       </div>
     </div>
   );
@@ -45,24 +89,119 @@ class App extends React.Component {
     super(props);
     this.state = {
       keyset: characterSet.map((row) => row.map((item) => item.char)),
+      topPanelkeys: topPanel.map((item) => item.char),
+      rightPanelkeys: rightPanel.map((item) => item.char),
+      leftPanelkeys: leftPanal.map((item) => item.char),
+      bottomPanelkeys: bottomPanel.map((item) => item.char),
       text: "மனிதக் குடும்பத்தினைச் சேர்ந்த யாவரதும் உள்ளார்ந்த மரியாதையையும், அவர்கள்",
+      crossMode: false,
+      crossModeBuffer: "",
     };
     this.onKeySelect = this.onKeySelect.bind(this);
+    this.onKeyUp = this.onKeyUp.bind(this);
+    this.onKeyDown = this.onKeyDown.bind(this);
+  }
+
+  onKeyDown(e) {
+    e.preventDefault();
+    console.dir(e);
+    if (e.key === "Shift") {
+      this.setState((state) => ({
+        ...state,
+        crossMode: true,
+      }));
+    }
+    if (e.key === "Backspace") {
+      this.setState((state) => ({
+        ...state,
+        text: this.state.text.slice(0, this.state.text.length - 1),
+      }));
+    }
+    if (e.key === " ") {
+      this.setState((state) => ({
+        ...state,
+        text: this.state.text.concat(" "),
+      }));
+    }
+  }
+  toRealConsonant(charToBeAdded) {
+    return realConsonantSet[charToBeAdded];
+  }
+  isConsonant(charToBeAdded) {
+    return consonantSet.some((item) => item.char === charToBeAdded);
+  }
+  combineChars(charToBeAdded, crossModeBuffer) {
+    return crossModeBuffer != ""
+      ? cartesianProductSet[crossModeBuffer][charToBeAdded]
+      : charToBeAdded;
+  }
+
+  onKeyUp(e) {
+    e.preventDefault();
+    if (e.key === "Shift") {
+      this.setState((state) => ({
+        ...state,
+        crossMode: false,
+      }));
+    }
   }
   onKeySelect(e) {
-    console.log(e.target.innerText);
+    let charToBeAdded = e.target.innerText;
 
-    this.setState((state) => ({
-      ...state,
-      text: state.text.concat(e.target.innerText),
-    }));
+    if (charToBeAdded == "SPACE") {
+      charToBeAdded = " ";
+    }
+    if (this.state.crossMode) {
+      console.log(
+        e.target.innerText +
+          "crossMode :" +
+          this.state.crossMode +
+          charToBeAdded +
+          " is Consonant " +
+          this.isConsonant(charToBeAdded) +
+          " crossModeBuffer " +
+          this.state.crossModeBuffer +
+          " isTruconsonant " +
+          this.toRealConsonant(charToBeAdded)
+      );
+
+      this.isConsonant(charToBeAdded)
+        ? this.state.crossModeBuffer === charToBeAdded
+          ? this.setState((state) => ({
+              ...state,
+              crossModeBuffer: "",
+              text: state.text.concat(this.toRealConsonant(charToBeAdded)),
+            }))
+          : this.setState((state) => ({
+              ...state,
+              crossModeBuffer: charToBeAdded,
+            }))
+        : this.setState((state) => ({
+            ...state,
+            text: state.text.concat(
+              this.combineChars(charToBeAdded, this.state.crossModeBuffer)
+            ),
+          }));
+    } else
+      this.setState((state) => ({
+        ...state,
+        text: state.text.concat(charToBeAdded),
+      }));
   }
   render() {
     return (
-      <div>
+      <div onKeyDown={this.onKeyDown} onKeyUp={this.onKeyUp}>
         <TextArea text={this.state.text} />
         <div id="seperator"></div>
-        <KeyPad keyset={this.state.keyset} onKeySelect={this.onKeySelect} />
+        <KeyPad
+          keyset={this.state.keyset}
+          onKeySelect={this.onKeySelect}
+          topPanelkeys={this.state.topPanelkeys}
+          leftPanelkeys={this.state.leftPanelkeys}
+          bottomPanelkeys={this.state.bottomPanelkeys}
+          rightPanelkeys={this.state.rightPanelkeys}
+          crossMode={this.crossMode}
+        />
       </div>
     );
   }
